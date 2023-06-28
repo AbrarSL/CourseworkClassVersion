9978503ff1a1d0054adfc26b9d9967e19a67c72a
AbrarSL <abrarsl2002@gmail.com>
2023-06-28 15:57:17 +0530

    Implement customer class

--- src/main/java/com/github/abrarsl/Customer.java
+++ src/main/java/com/github/abrarsl/Customer.java
@@ -1,4 +1,25 @@
 package com.github.abrarsl;
 
 public class Customer {
+    private final String firstName;
+    private final String lastName;
+    private final int burgersRequired;
+
+    public Customer(String firstName, String lastName, int burgersRequired) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.burgersRequired = burgersRequired;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getBurgersRequired() {
+        return this.burgersRequired;
+    }
 }
