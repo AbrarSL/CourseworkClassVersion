38c581589e4cceff5260c9c9ff92529b9383f1e4
AbrarSL <abrarsl2002@gmail.com>
2023-07-01 17:46:54 +0530

    Implement FoodQueue class

--- src/main/java/com/github/abrarsl/FoodQueue.java
+++ src/main/java/com/github/abrarsl/FoodQueue.java
@@ -1,4 +1,33 @@
 package com.github.abrarsl;
 
+import java.util.ArrayList;
+
 public class FoodQueue {
+    private final ArrayList<Customer> queue;
+    private final int queueLength;
+
+    public FoodQueue(int queueLength) {
+        this.queue = new ArrayList<>(queueLength);
+        this.queueLength = queueLength;
+    }
+
+    public int getQueueLength() {
+        return this.queueLength;
+    }
+
+    public Customer removeCustomer(int customerIndex) {
+        if (customerIndex < 0 || customerIndex >= this.queueLength) {
+            return null;
+        }
+
+        return this.queue.remove(customerIndex);
+    }
+
+    public boolean addCustomer(Customer customer) {
+        if (this.queue.size() >= this.queueLength) {
+            return false;
+        }
+
+        return this.queue.add(customer);
+    }
 }
